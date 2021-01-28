a3d1a3a7295066b7711ab9a68de696fd0fe53e96
Muhammad Huzaifa Khan <47402181+MHuzaifaKhan97@users.noreply.github.com>
2021-01-28 23:42:04 +0500

    Custom DatePicker with Dialog.

---
+++ cust_dp_dialog/lib/main.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'dialogs/datePickerDialog.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  String result;
+  MyHomePage({this.result = ""});
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Custom Dialog Date Picker'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Picked Date: ${widget.result}',
+              style: TextStyle(fontSize: 24),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          dynamic expected = await showDialog(
+              context: context, builder: (context) => DatePickerDialog());
+          setState(() {
+            widget.result = expected;
+          });
+        },
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
